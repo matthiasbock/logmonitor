9b021c3f9d4e0aea2108c44c5b9ef5b6965ab124
Matthias Bock <mail@matthiasbock.net>
2019-10-23 21:01:31 +0200

    Added second rule and whitelisted interoberlin.de

--- f2b-exim.py
+++ f2b-exim.py
@@ -22,8 +22,19 @@ def matchRule1(line):
 		return line[a+1:b]
 	return None
 
+def matchRule2(line):
+	if line == "test":
+		return "2019-10-13 20:23:27 1iNLn5-0003SK-Ck H=testdomain.de [12.145.178.190] X=TLS1.2:ECDHE_RSA_AES_128_GCM_SHA256:128 CV=no F=<waldokjsckjg@losefatplans.co> rejected after DATA: Message rejected as spam."
+	line = line.lower()
+	if (line.find("rejected after data") > -1) and (line.find("spam") > -1):
+		a = line.find("[")
+		b = line.find("]")
+		return line[a+1:b]
+	return None
+
 rules = [
-	matchRule1
+	matchRule1,
+	matchRule2
 	]
 
 #
@@ -53,8 +64,9 @@ for rule in rules:
 #
 def log(s):
 	s = s.strip()
+	date = subprocess.Popen(["date", "+%Y-%m-%d %H:%M:%S"], stdout=subprocess.PIPE, encoding="utf8").communicate()[0].strip()
 	f = open(logfile, "a")
-	f.write(s + "\n")
+	f.write(date + " f2b-exim.py: " + s + "\n")
 	f.close()
 
 
@@ -62,6 +74,10 @@ def log(s):
 # Ban a certain IP from contacting our Exim again (typically for 24h)
 #
 def ban(ip):
+	if (ip == "37.120.163.112"):
+		print("Not banning whitelisted IP 37.120.163.112 (interoberlin.de).")
+		log("Not banning whitelisted IP 37.120.163.112 (interoberlin.de).")
+		return
 	print("Banning IP {:s} for 24h ...".format(ip))
 	subprocess.Popen(["fail2ban-client", "set", "exim", "banip", ip]).wait()
 	log("IP {:s} banned for 24h.".format(ip))
@@ -81,7 +97,7 @@ def evaluate(line):
 
 print("Monitoring {:s} ... Press Ctrl+C to cancel.".format(logfile))
 f = subprocess.Popen(
-	['tail', '-fn50', logfile],
+	['tail', '-fn150', logfile],
 	stdout=subprocess.PIPE,
 	stderr=subprocess.PIPE,
 	encoding='utf8'
